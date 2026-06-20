fab4fba679167c7b4714e674875033dd15392a7c
Mahmoud Maher <tea0mah2009@gmail.com>
2026-06-20 14:46:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,13 @@ $page_code = true;
 
 if ($page_code == true) {
 
-    echo '<p style="font-size: 100px; font-family: Arial, Helvetica, sans-serif; text-align: center; margin-top: 200px;"> Development underway </p>';
+    echo '<p style="font-size: 100px; 
+                    font-family: Arial, Helvetica, sans-serif; 
+                    text-align: center; 
+                    margin-top: 200px; 
+                    font-weight: 700;
+                    color: #d66247;"
+                    > Development underway </p>';
 
 } else {
 
