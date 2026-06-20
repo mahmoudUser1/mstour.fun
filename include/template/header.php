b3d769a5132fbc139ddb6dab69da16d3d4bd9612
Mahmoud Maher <tea0mah2009@gmail.com>
2026-06-20 12:43:54 +0300

    create new

---
+++ include/template/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>admin page</title>
+
+    <link rel="stylesheet" href="<?php echo $css ?>bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo $css ?>sweetalart.min.css">
+    <link rel="stylesheet" href="<?php echo $css ?>all.min.css">
+    <link rel="stylesheet" href="<?php echo $css ?>main.css">
+
+</head>
+
+<body>
