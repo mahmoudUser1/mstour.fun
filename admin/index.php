14ffb9255810a38a808a23bd4d933f4183ce0a91
Mahmoud Maher <tea0mah2009@gmail.com>
2026-06-20 16:48:07 +0300

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$page_code = true;
+
+if ($page_code == true) {
+
+    echo '<p style="font-size: 100px; 
+                    font-family: Arial, Helvetica, sans-serif; 
+                    text-align: center; 
+                    margin-top: 200px; 
+                    font-weight: 700;
+                    color: #ff0000;
+                    letter-spacing: 5px;"
+                    > Development underway ... </p>';
+
+} else {
+
+
+
+}
