c2cec177c6b688353e1dd7b3dd561229850ef7d0
Mahmoud Maher <tea0mah2009@gmail.com>
2026-06-27 16:26:29 +0300

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -112,17 +112,13 @@ function isAllowedFileType($filename) {
 // دالة إنشاء مجلد آمن
 function createUploadDirectory($path) {
     if (!is_dir($path)) {
-<<<<<<< HEAD
-        if (!mkdir($path, 0777, true)) {
+        if (!mkdir($path, 0755, true)) {
             error_log("Failed to create directory: " . $path);
             return false;
         }
     }
+
     return true;
-=======
-        mkdir($path, 0755, true);
-    }
->>>>>>> 03a7eaf3cc07107b36c95589b7bd91e4012d78ed
 }
 
 // دالة حفظ الملف
@@ -148,7 +144,6 @@ function saveUploadedFile($file, $userId) {
     $uploadDir = UPLOAD_DIR . $userId . '/';
     createUploadDirectory($uploadDir);
     
-<<<<<<< HEAD
     $ext = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
     
     // تأمين الملفات الحساسة (مثل PHP) بإضافة امتداد إضافي أو تغيير اسمها
@@ -160,9 +155,7 @@ function saveUploadedFile($file, $userId) {
         $filename = $safe_filename . '.' . $ext;
     }
     
-=======
     $filename = uniqid() . '_' . basename($file['name']);
->>>>>>> 03a7eaf3cc07107b36c95589b7bd91e4012d78ed
     $filepath = $uploadDir . $filename;
     
     // نقل الملف
